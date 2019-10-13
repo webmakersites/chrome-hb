ee11504a8e84700b9971ae064dde110a856ae35d
PixelBoy <48217893+PixelBoyOfficial@users.noreply.github.com>
2019-10-13 11:46:47 -0600

    Add files via upload

---
+++ appfiles/config.js
@@ -0,0 +1,15 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Chromebrew",
+   "version": "1.0",
+   "homepage": "https://pixelboyofficial.github.io/chrome-hb/",
+   "enableNavBttns": true,
+   "enableHomeBttn": false,
+   "enableReloadBttn": true,
+   "enableLogoutBttn": false,
+   "sessionDataTimeoutTime": 1,
+   "sessionTimeoutTime": 10,
+   "termsOfService": "TERMS OF SERVICE!\u000aChromebrew is not responsible for your usage. Use responsibly!",
+   "kioskEnabled": false
+};
